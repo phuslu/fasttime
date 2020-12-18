18ab163531a7a92010c72519a1bfd8a866f12cfd
phuslu <phus.lu@gmail.com>
2020-12-18 15:13:31 +0800

    add timestamp with ms

--- time.go
+++ time.go
@@ -1,5 +1,6 @@
 package fasttime
 
+// Timestamp formats unix timestamp.
 func Timestamp() string {
 	return string(timestamp(make([]byte, 10)))
 }
@@ -7,6 +8,40 @@ func Timestamp() string {
 func timestamp(b []byte) []byte {
 	sec, _ := walltime()
 	a := sec % 100 * 2
+	sec /= 100
+	b[9] = tab[a+1]
+	b[8] = tab[a]
+	a = sec % 100 * 2
+	sec /= 100
+	b[7] = tab[a+1]
+	b[6] = tab[a]
+	a = sec % 100 * 2
+	sec /= 100
+	b[5] = tab[a+1]
+	b[4] = tab[a]
+	a = sec % 100 * 2
+	sec /= 100
+	b[3] = tab[a+1]
+	b[2] = tab[a]
+	a = sec % 100 * 2
+	b[1] = tab[a+1]
+	b[0] = tab[a]
+	return b
+}
+
+// TimestampMS formats unix timestamp with milli seconds.
+func TimestampMS() string {
+	return string(timestampms(make([]byte, 13)))
+}
+
+func timestampms(b []byte) []byte {
+	sec, nsec := walltime()
+	ms := int64(nsec) / 1000000
+	a := ms % 100 * 2
+	b[12] = tab[a+1]
+	b[11] = tab[a]
+	b[10] = byte('0' + ms/100)
+	a = sec % 100 * 2
 	sec /= 100
 	b[9] = tab[a+1]
 	b[8] = tab[a]
